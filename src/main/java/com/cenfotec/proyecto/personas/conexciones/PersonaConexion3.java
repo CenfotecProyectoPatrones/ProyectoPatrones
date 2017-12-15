70ae34e7cfa8e4e8db4d310cf9919dbf5322f1c7
melecr <melegatti@gmail.com>
2017-12-15 16:10:52 -0600

    FuncionarioMunicipal
    FuncionarioMunicipal

---
+++ src/main/java/com/cenfotec/proyecto/personas/conexciones/PersonaConexion3.java
@@ -0,0 +1,45 @@
+package com.cenfotec.proyecto.personas.conexciones;
+
+import java.io.File;
+import java.io.IOException;
+
+import com.cenfotec.proyecto.documentos.ActualizacionInformacionContribuyente;
+import com.cenfotec.proyecto.documentos.Documento;
+import com.cenfotec.proyecto.personas.Persona;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class PersonaConexion3 {
+
+	public static void guardarJson(Persona documento) {
+		ObjectMapper mapper = new ObjectMapper();
+		try {
+			mapper.writeValue(new File("Json//Persona.json"), documento);
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Persona devolverObjecto() {
+		ObjectMapper mapper = new ObjectMapper();
+		File jsonFile = new File("Json//Persona.json");
+		Persona doc=null;
+		try {
+			mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+			doc=mapper.readValue(jsonFile,Persona.class);;
+		} catch (JsonGenerationException e) {
+			e.printStackTrace();
+		} catch (JsonMappingException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return doc;
+	}
+}
